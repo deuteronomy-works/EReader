f7b3f1ac438abb2cea90e6d7b1fb728d2a7d754c
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2024-12-27 16:06:01 +0000

    Update handle_types.py

--- EReader/handle_types.py
+++ EReader/handle_types.py
@@ -5,29 +5,31 @@ class HandleTypes:
     @staticmethod
     def save_bytes_content(filename, content):
         folder, name = os.path.split(filename)
-        if folder:
+        if folder and  not os.path.exists(folder):
             os.makedirs(folder)
+
         with open(filename, mode='wb') as raw_file:
             raw_file.write(content)
     
     @staticmethod
     def save_content(filename, content):
         folder, name = os.path.split(filename)
-        if folder:
+        if folder and  not os.path.exists(folder):
             os.makedirs(folder)
+        
         with open(filename, mode='wb') as write_file:
             write_file.write(content)
 
     @staticmethod
     def ITEM_UNKNOWN(item):
-        print(item.get_name())
-        content = item.get_content()
+        HandleTypes.save_content(item.get_name(),item.get_content())
+        content = ""
         return content
 
     @staticmethod
     def ITEM_IMAGE(item):
-        print(item.get_name())
-        content = item.get_content()
+        HandleTypes.save_content(item.get_name(),item.get_content())
+        content = ""
         return content
 
     @staticmethod
@@ -38,54 +40,54 @@ class HandleTypes:
 
     @staticmethod
     def ITEM_SCRIPT(item):
-        print(item.get_name())
-        content = item.get_content()
+        HandleTypes.save_content(item.get_name(),item.get_content())
+        content = ""
         return content
 
     @staticmethod
     def ITEM_NAVIGATION(item):
-        print(item.get_name())
-        content = item.get_content()
+        HandleTypes.save_content(item.get_name(),item.get_content())
+        content = ""
         return content
 
     @staticmethod
     def ITEM_VECTOR(item):
-        print(item.get_name())
-        content = item.get_content()
+        HandleTypes.save_content(item.get_name(),item.get_content())
+        content = ""
         return content
 
     @staticmethod
     def ITEM_FONT(item):
-        print(item.get_name())
-        content = item.get_content()
+        HandleTypes.save_content(item.get_name(),item.get_content())
+        content = ""
         return content
 
     @staticmethod
     def ITEM_VIDEO(item):
-        print(item.get_name())
-        content = item.get_content()
+        HandleTypes.save_content(item.get_name(),item.get_content())
+        content = ""
         return content
 
     @staticmethod
     def ITEM_AUDIO(item):
-        print(item.get_name())
-        content = item.get_content()
+        HandleTypes.save_content(item.get_name(),item.get_content())
+        content = ""
         return content
 
     @staticmethod
     def ITEM_DOCUMENT(item):
-        print(item.get_name())
-        content = item.get_body_content()
+        HandleTypes.save_content(item.get_name(),item.get_content())
+        content = ""
         return content
 
     @staticmethod
     def ITEM_COVER(item):
-        print(item.get_name())
-        content = item.get_content()
+        HandleTypes.save_content(item.get_name(),item.get_content())
+        content = ""
         return content
 
     @staticmethod
     def ITEM_SMIL(item):
-        print(item.get_name())
-        content = item.get_content()
+        HandleTypes.save_content(item.get_name(),item.get_content())
+        content = ""
         return content
