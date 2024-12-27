eb7252a57efd4906c2c76395a7214209eaaa4441
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2024-12-27 13:35:09 +0000

    handle various types

--- EReader/__init__.py
+++ EReader/__init__.py
@@ -1,16 +1,38 @@
 import ebooklib
 from ebooklib import epub
+from handle_types import HandleTypes
 
-fileIn = "./ex1.epub"
-fileOut = "out01.html"
+fileIn = "./ex2.epub"
+fileOut = "tags_view.txt"
+
 
 book = epub.read_epub(fileIn)
 content = ""
+tags = {
+      ebooklib.ITEM_UNKNOWN : HandleTypes.ITEM_UNKNOWN,
+ebooklib.ITEM_IMAGE : HandleTypes.ITEM_IMAGE,
+ebooklib.ITEM_STYLE : HandleTypes.ITEM_STYLE,
+ebooklib.ITEM_SCRIPT: HandleTypes.ITEM_SCRIPT,
+ebooklib.ITEM_NAVIGATION: HandleTypes.ITEM_NAVIGATION,
+ebooklib.ITEM_VECTOR: HandleTypes.ITEM_VECTOR,
+ebooklib.ITEM_FONT: HandleTypes.ITEM_FONT,
+ebooklib.ITEM_VIDEO: HandleTypes.ITEM_VIDEO,
+ebooklib.ITEM_AUDIO: HandleTypes.ITEM_AUDIO,
+ebooklib.ITEM_DOCUMENT: HandleTypes.ITEM_DOCUMENT,
+ebooklib.ITEM_COVER: HandleTypes.ITEM_COVER,
+ebooklib.ITEM_SMIL: HandleTypes.ITEM_SMIL
+}
 
+ind = 0
 for item in book.get_items():
-    if item.get_type() == ebooklib.ITEM_DOCUMENT:
+    ind += 1
+    if ind < 5:
+          continue
+    content = tags[item.get_type()](item)
+    """ if item.get_type() == ebooklib.ITEM_DOCUMENT:
         bodyContent = item.get_body_content().decode()
-        content += bodyContent
+        content += bodyContent """
+    break
 
-with open(fileOut, 'w', encoding='utf-8') as fout:
+with open(fileOut, 'wb') as fout:
         fout.write(content)
---
+++ EReader/handle_types.py
@@ -0,0 +1,76 @@
+
+
+class HandleTypes:
+
+
+    @staticmethod
+    def ITEM_UNKNOWN(item):
+        print(item.get_name())
+        content = item.get_content()
+        return content
+
+    @staticmethod
+    def ITEM_IMAGE(item):
+        print(item.get_name())
+        content = item.get_content()
+        return content
+
+    @staticmethod
+    def ITEM_STYLE(item) :
+        print(item.get_name())
+        content = item.get_content()
+        return content
+
+    @staticmethod
+    def ITEM_SCRIPT(item):
+        print(item.get_name())
+        content = item.get_content()
+        return content
+
+    @staticmethod
+    def ITEM_NAVIGATION(item):
+        print(item.get_name())
+        content = item.get_content()
+        return content
+
+    @staticmethod
+    def ITEM_VECTOR(item):
+        print(item.get_name())
+        content = item.get_content()
+        return content
+
+    @staticmethod
+    def ITEM_FONT(item):
+        print(item.get_name())
+        content = item.get_content()
+        return content
+
+    @staticmethod
+    def ITEM_VIDEO(item):
+        print(item.get_name())
+        content = item.get_content()
+        return content
+
+    @staticmethod
+    def ITEM_AUDIO(item):
+        print(item.get_name())
+        content = item.get_content()
+        return content
+
+    @staticmethod
+    def ITEM_DOCUMENT(item):
+        print(item.get_name())
+        content = item.get_body_content()
+        return content
+
+    @staticmethod
+    def ITEM_COVER(item):
+        print(item.get_name())
+        content = item.get_content()
+        return content
+
+    @staticmethod
+    def ITEM_SMIL(item):
+        print(item.get_name())
+        content = item.get_content()
+        return content
