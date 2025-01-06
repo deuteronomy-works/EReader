9276ce9b6cf6bc7354b1b44459f1fc4920228441
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2025-01-06 16:55:24 +0000

    Update misc.py

--- EReader/misc.py
+++ EReader/misc.py
@@ -1,12 +1,16 @@
 
 
-READER_CSS_PATH = "./reader_files/css/reader.css"
+READER_CSS_PATH = "./css/reader.css"
+USER_CONTENT_FOLDER= "../user_contents/"
 
 
-def reader_html_replacer(index_file: str, nav_file: str) -> str:
+def reader_html_replacer(index_file: str, nav_file: str) -> None:
 
-    with open("./reader.html", 'w') as reader_file:
+    with open("./reader.html", 'r') as reader_file:
         replaced = reader_file.read()
+
+    index_file = USER_CONTENT_FOLDER + index_file
+    nav_file = USER_CONTENT_FOLDER + nav_file
 
     # replace css
     replaced = replaced.replace("{{CSS_FILE}}", READER_CSS_PATH)
@@ -17,4 +21,5 @@ def reader_html_replacer(index_file: str, nav_file: str) -> str:
     # replace nav
     replaced = replaced.replace("{{NAV_FILE}}", nav_file)
 
-    return replaced
+    with open("./reader_files/reader.html", 'w') as reader_file:
+        reader_file.write(replaced)
